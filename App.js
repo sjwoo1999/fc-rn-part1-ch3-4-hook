e55a331af8b5cb8af6b2f349b3db7ac3eb98b6f7
_woo_s.j <sjwoo1999@korea.ac.kr>
2023-07-04 14:54:11 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// 시작 전 App.js 코드
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import StateWithClassComponent from "./StateWithClassComponent";
+import StateWithFuctionalComponent from "./StateWithFuctionalComponent";
+import UseEffectWithClassComponent from "./UseEffectWithClassComponent";
+import UseEffectWithFunctionalComponent from "./UseEffectWithFunctionalComponent";
+import CustomHook from "./CustomHook";
 
 export default function App() {
+  const [isTrue, setIsTrue] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StateWithClassComponent /> */}
+      {/* <StateWithFuctionalComponent /> */}
+      {/* {isTrue ? <UseEffectWithClassComponent /> : null} */}
+      {/* <UseEffectWithFunctionalComponent /> */}
+      {/* <Button title="toggle" onPress={() => setIsTrue(!isTrue)} /> */}
+
+      <CustomHook />
     </View>
   );
 }
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
